00b192cf016bba0bdb77a31746e8783ad950d875
Imtiaz <134070730+amimtiaz@users.noreply.github.com>
2024-10-12 15:20:51 +0600

    Update Auto_Slider.java

--- Auto_Slider.java
+++ Auto_Slider.java
@@ -24,6 +24,24 @@
     */
 
 
+  // slider item -- example
+  public class SliderItem {
+    private int image;
+
+    public SliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
+
 
 // Slider Adapter
 
